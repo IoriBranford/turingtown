d29423c3eda38f66327ac20d9a3cf840bce267df
IoriBranford <ioribranford@gmail.com>
2023-06-16 13:50:56 -0700

    Show only last 2 messages including loading.

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -121,7 +121,7 @@ export function Chat() {
 
   return (
     <div className="rounded-2xl border-zinc-100  lg:border lg:p-6">
-      {messages.map(({ content, role }, index) => (
+      {messages.slice(loading ? -1 : -2).map(({ content, role }, index) => (
         <ChatLine key={index} role={role} content={content} />
       ))}
 
